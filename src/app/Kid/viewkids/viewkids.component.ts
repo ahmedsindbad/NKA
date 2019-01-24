1b7b383ad6585663acf78d2a21d20481f8206d81
ahmedsindbad <ahmedkamal19855@gmail.com>
2019-01-24 21:23:51 +0200

    fix viewkids with Details

--- src/app/Kid/viewkids/viewkids.component.ts
+++ src/app/Kid/viewkids/viewkids.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { KidService} from '../../services/kid.service';
 import { KID } from '../KID';
+import { EATING } from '../../Activity/EATING';
+import { EVENT } from '../../Activity/EVENT';
+import { CLASS } from '../../Activity/CLASS';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { onErrorResumeNextStatic } from 'rxjs/internal/operators/onErrorResumeNext';
+
 
 
 @Component({
@@ -14,22 +17,35 @@ export class ViewkidsComponent implements OnInit {
 
   // variables
   kids: KID[];
-  id: any;
+  eatings: EATING[];
+  events: EVENT[];
+  classes: CLASS[];
+
+  kidid: any;
   // const
   constructor(public kidService: KidService, private route: ActivatedRoute) {
     this.route.paramMap.subscribe((param: ParamMap) => {
-      this.id = param.get('id');
+      this.kidid = param.get('id');
     });
   }
 
   ngOnInit() {
 
-    this.kidService.GetItem().subscribe(Kids => {
+    this.kidService.GetSampleKids(this.kidid).subscribe(Kids => {
       // console.log(Kids);
       this.kids = Kids;
     });
+
+
   }
-  onTest() {
-    alert(this.id);
+  onDetails(KCGUID: string, kidid: string) {
+    this.kidService.GetEventKids(KCGUID).subscribe(events => {
+      this.events = events;
+    });
+    this.kidService.GetEatKids(kidid).subscribe(eats => {
+      this.eatings = eats;
+      console.log(this.eatings);
+    });
+
     }
 }
--- src/app/services/kid.service.ts
+++ src/app/services/kid.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { KID } from '../Kid/KID';
+import { EATING } from '../Activity/EATING';
+import { EVENT } from '../Activity/EVENT';
+import { CLASS } from '../Activity/CLASS';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators/index';
 
@@ -12,12 +15,19 @@ export class KidService {
   KidsCollection: AngularFirestoreCollection<KID>;
   Kids: Observable<any>;
 
+  EventCollection: AngularFirestoreCollection<EVENT>;
+  Events: Observable<any>;
+
+  EatCollection: AngularFirestoreCollection<EATING>;
+  Eats: Observable<any>;
+
+  date = new Date().toDateString();
   constructor(public db: AngularFirestore) {
     // this.Kids = db.collection('Kids').valueChanges();
   }
-
-  GetItem() {
-    this.KidsCollection = this.db.collection('Kids', ref => ref.orderBy('id', 'asc'));
+  // Sample kids data
+  GetSampleKids(kidid: string) {
+    this.KidsCollection = this.db.collection('Kids', ref => ref.where('id', '==', kidid));
 
     this.Kids = this.KidsCollection.snapshotChanges().pipe(map(changes => {
       return changes.map(a => {
@@ -26,6 +36,30 @@ export class KidService {
         return data;
       }); }));
     return this.Kids;
+  }
+  //  Events Data
+  GetEventKids(classid: string) {
+    this.EventCollection = this.db.collection('EVENTS/' + this.date + '/' + classid, ref => ref.orderBy('timefrom', 'asc'));
+
+    this.Events = this.EventCollection.snapshotChanges().pipe(map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as EVENT;
+        data.id = a.payload.doc.id;
+        return data;
+      }); }));
+    return this.Events;
+  }
+
+  GetEatKids(kidid: string) {
+    this.EatCollection = this.db.collection('EATINGS/' + this.date + '/' + kidid);
+
+    this.Eats = this.EatCollection.snapshotChanges().pipe(map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as EATING;
+        data.key = a.payload.doc.id;
+        return data;
+      }); }));
+    return this.Eats;
   }
 
   addkid(kid: KID) {
